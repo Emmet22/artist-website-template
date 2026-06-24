4aeea73f410e376efb3f34213a7d0b3aca592991
Emmet Casey <emmetfcasey@gmail.com>
2026-06-24 18:43:42 +0100

    Removed Vite sample template and added a sample Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+      <h2>This is the navbar</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
