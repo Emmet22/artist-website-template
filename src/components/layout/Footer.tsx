6fd730d4a48fe6332e17a412ad01e92d4d5c6dfb
Emmet Casey <emmetfcasey@gmail.com>
2026-07-01 18:38:12 +0100

    Added sample content to the footer (branding, sample media links, page links, and copyright). Created Footer component css file and laid out all the content apropriately

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,35 @@
+import styles from './Footer.module.css';
+
 function Footer() {
     return (
-        <div style={{backgroundColor: "grey"}}>
-            <h3>THIS IS A FOOTER</h3>
-            <p>Copyright Emmet Casey 2026</p>
+        <div className={styles.footer}>
+            <div className={styles.brand}>
+                <div className={styles.logo}>LOGO</div>
+                <h3>Original Art Each Unique</h3>
+            </div>
+
+            <div className={styles.footerContent}>
+                
+
+                <div className={styles.mediaLinks}>
+                    <ul>
+                        <li><a href="#">INSTAGRAM</a></li>
+                        <li><a href="#">FACEBOOK</a></li>
+                        <li><a href="#">TIKTOK</a></li>
+                        <li><a href="#">PINTREST</a></li>
+                    </ul>
+                </div>
+
+                <div className={styles.footerLinks}>
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">GALLERY</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p className={styles.copyright}>Copyright Emmet Casey 2026</p>
         </div>
     );
 }
