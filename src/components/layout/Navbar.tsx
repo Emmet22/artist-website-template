ac3b6c2027c968dbb0537278c4e13ba76c7f0fef
Emmet Casey <emmetfcasey@gmail.com>
2026-07-01 18:08:20 +0100

    Added Navbar component styling with an appropriate css module file. Added sample logo and links, and alligned them appropraitely

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,18 @@
+import styles from './Navbar.module.css';
+
 function Navbar() {
   return (
-    <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
-      <h2>This is the navbar</h2>
-    </nav>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <p>LOGO</p>
+      </div>
+      <ul className={styles.navLinks}>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+      </nav>
   );
 }
 
