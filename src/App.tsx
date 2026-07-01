e092463f7e1e8e086c0fb97922357f6aa1e2c75d
Emmet Casey <emmetfcasey@gmail.com>
2026-07-01 21:01:06 +0100

    Added more sections to homepage (currently App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/common/Hero";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Collection from "./components/artwork/Collection";
+import Section from "./components/common/Section";
 import { sampleCollections } from "./data/SampleCollectionData";
 import "./styles/App.css";
 
@@ -13,8 +14,43 @@ function App() {
       <main>
         <Hero />
 
-        <p>This is a sample collection</p>
-        {/* using the map function to make use of the sample collection data */}
+        <Section>
+          <h3>Featured Collections</h3>
+          <Collection 
+            key="1"
+            name="Sample Collection"
+            description="This is a sample collection description."
+            pieces={[
+              { id: 1, imageURL: "sampleURL1", description: "Sample desc 1" },
+              { id: 2, imageURL: "sampleURL2", description: "Sample desc 2" },
+              { id: 3, imageURL: "sampleURL3", description: "Sample desc 3" },
+            ]}
+          />  
+        </Section>
+
+        <Section> 
+            <h3>Why each piece is unique</h3>
+            <p>Each piece is unique sample text sample description etc etc etc</p>
+
+        </Section>
+
+        <Section>
+          <h3>The Process</h3>
+          <ul>
+            <li><p>Find piece you like</p></li>
+            <li><p>Enquire to artist to cmossion a piece from the collection</p></li>
+            <li><p>Artist approves commission</p></li>
+            <li><p>You recieve your piece, with a certificate of uniqueness</p></li>
+          </ul>
+        </Section>
+
+        <Section> 
+          <h3>Be the first to hear about new collections</h3>
+          <p>EMAIL LIST SIGN UP - WORK IN PRGRESS</p>
+        </Section>
+
+        {/* OLD CODE
+        { using the map function to make use of the sample collection data 
         {
           sampleCollections.map((collection) => (
             <Collection 
@@ -25,7 +61,7 @@ function App() {
             />
           ))
         }      
-  
+        */}
       </main>
     <Footer />
       
