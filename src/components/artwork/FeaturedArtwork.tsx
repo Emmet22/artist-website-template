1961460e4c784ea6bc8b23c33f55dbf9632f1ca2
Emmet Casey <emmetfcasey@gmail.com>
2026-07-11 13:23:02 +0100

    #13 Added description and browse all button to FeaturedArtwork component for future use

--- src/components/artwork/FeaturedArtwork.tsx
+++ src/components/artwork/FeaturedArtwork.tsx
@@ -6,7 +6,7 @@ function FeaturedArtwork() {
     return (
         <div className={styles.featuredArtwork}>
             <h2>Avaliable to purchase</h2>
-
+            <p>Newest artwork ready to ship immediately</p>
             <div className={styles.pieces}>
                 <ArtPiece  
                     imageURL= "https://placehold.co/150x150" 
@@ -29,7 +29,7 @@ function FeaturedArtwork() {
                     description= "Sample desc 1"
                 />
             </div>
-
+            <button className="btn">Browse All</button>
         </div>
     );
 }
