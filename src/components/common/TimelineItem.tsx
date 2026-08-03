0240026bbbe8d3338a9c0dcd6f455a34c7b6412f
Emmet Casey <emmetfcasey@gmail.com>
2026-08-03 17:21:37 +0100

    #31 added slide in animations to the timeline

--- src/components/common/TimelineItem.tsx
+++ src/components/common/TimelineItem.tsx
@@ -1,6 +1,8 @@
 import type {TimelineStep} from "../../types/TimelineStep";
 import styles from "./TimelineItem.module.css";
 
+import { useEffect, useRef, useState } from "react";
+
 interface TimelineItemProps {
   step: TimelineStep;
   index: number;
@@ -9,10 +11,44 @@ interface TimelineItemProps {
 function TimelineItem({step, index}: TimelineItemProps) {
     const isLeft = index % 2 === 0;
   
+    // for animations
+    const itemRef = useRef<HTMLDivElement>(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.disconnect();
+                }
+            },
+            {
+                threshold: 0.2,
+            }
+        );  
+
+        const currentItem = itemRef.current;
+
+        if (currentItem) {
+            observer.observe(currentItem);
+        }
+
+        return () => {
+            if (currentItem) {
+                observer.unobserve(currentItem);
+            }
+        };
+    }, []);
+
     return (
-        <div className={`${styles.item} ${
-            isLeft ? styles.left : styles.right
-        }`}
+        <div 
+        ref={itemRef}
+        className={`
+            ${styles.item} 
+            ${isLeft ? styles.left : styles.right}
+            ${isVisible ? styles.show : ""}
+        `}
         >
             {
                 isLeft && (            
