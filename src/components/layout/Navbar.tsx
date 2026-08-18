c678a63aa30df3d179f9511ba1e5fb391415ab37
Emmet Casey <emmetfcasey@gmail.com>
2026-08-18 20:46:15 +0100

    #5 first attempt at navbar responsiveness, added react state for hamburger button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,27 @@
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 
 import styles from './Navbar.module.css';
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <p>LOGO</p>
       </div>
-      <ul className={styles.navLinks}>
+
+      <button
+        className={styles.menuButton}
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle navigation menu"
+      >
+        ☰
+      </button>
+
+      <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
         <li><Link to="/">HOME</Link></li>
         <li><Link to="/gallery">GALLERY</Link></li>
         <li><Link to="/commission">CREATE YOUR PIECE</Link></li>
